e6f28ec6d065ddc7a439ebdc0538d098b02a07c9
agu <wu_zhengu@qq.com>
2023-06-28 23:07:15 +0800

    1

---
+++ app/src/main/java/app/ui/LaunchPage.java
@@ -0,0 +1,14 @@
+package app.ui;
+
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LaunchPage extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(@Nullable Bundle bundle){
+        super.onCreate(bundle);
+    }
+}
